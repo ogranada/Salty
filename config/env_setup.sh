59ccfb08771a64762e5ed0b4b43a9bc4b0e9bf4c
Oscar Granada <oscar.andres.granada@gmail.com>
2021-06-24 23:33:12 -0500

    Adding env check for python2

--- config/env_setup.sh
+++ config/env_setup.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 source /usr/local/bin/virtualenvwrapper.sh
 
-if [ ! -d "$1/py2env" ]; then
-  mkvirtualenv py2env -p python2
-  workon py2env
-  pip2 install neovim
+if ! command -v python2 &> /dev/null
+then
+    echo "Avoiding to prepare Python2 Env."
+else
+  if [ ! -d "$1/py2env" ]; then
+    mkvirtualenv py2env -p python2
+    workon py2env
+    pip2 install neovim
+  fi
 fi
+
 
 if [ ! -d "$1/py3env" ]; then
   mkvirtualenv py3env -p python3
